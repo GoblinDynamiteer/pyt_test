509c25e64d4fdca904d72a7d02ccdca83a0d0db6
Johan Kämpe <kampe.johan@gmail.com>
2016-10-17 23:46:42 +0200

    Simple function test

---
+++ functions.py
@@ -0,0 +1,13 @@
+
+def area_tri(h,b): #definition of functions in python
+	return h*b/2 #return the area of a triangle
+
+hojd = int(input("Ange hojd: ")) #converts user input string -> int with int() and sets to variable
+bredd = int(input("Ange bredd: "))
+
+area = area_tri(hojd,bredd) #area is set to return value of the function
+
+print("Triangelns area ar",area) #not sure how to get åäöÅÄÖ to work with python scripts...
+
+#this also works, but makes it more unreadable
+print("Triangelns area ar",area_tri(int(input("Ange hojd: ")),int(input("Ange bredd: "))))
