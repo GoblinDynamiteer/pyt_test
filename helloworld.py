852cc514771c8aa0258f8b6c122a89490b9d5cce
Johan Kämpe <kampe.johan@gmail.com>
2016-10-17 23:27:44 +0200

    Added comments

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,3 @@
-text = "Hello World"
-print (text, "!")
-print (text.upper(), "!")
+text = "Hello World" #this is a comment!
+print (text, "!") #prints out the string in variable 'text' and adds an exclamaiton mark after
+print (text.upper(), "!") #this makes the tet in the variable 'text' uppercasse
