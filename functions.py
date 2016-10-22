89d3d078d8557945cccd87d9bc1574452717b501
Johan Kämpe <kampe.johan@gmail.com>
2016-10-22 15:35:21 +0200

    Added Code Encoding

--- functions.py
+++ functions.py
@@ -1,13 +1,13 @@
-
+# -*- coding: latin-1 -*-
 def area_tri(h,b): #definition of functions in python
 	return h*b/2 #return the area of a triangle
 
-hojd = int(input("Ange hojd: ")) #converts user input string -> int with int() and sets to variable
+hojd = int(input("Ange höjd: ")) #converts user input string -> int with int() and sets to variable
 bredd = int(input("Ange bredd: "))
 
 area = area_tri(hojd,bredd) #area is set to return value of the function
 
-print("Triangelns area ar",area) #not sure how to get åäöÅÄÖ to work with python scripts...
+print("Triangelns area är",area)
 
 #this also works, but makes it more unreadable
-print("Triangelns area ar",area_tri(int(input("Ange hojd: ")),int(input("Ange bredd: "))))
+print("Triangelns area är",area_tri(int(input("Ange höjd: ")),int(input("Ange bredd: "))))
