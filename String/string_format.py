8b74a96d64c042f76751296b0f50a6f7f17895ee
Johan Kämpe <kampe.johan@gmail.com>
2017-06-08 20:11:55 +0200

    Added more .format() tests

--- String/string_format.py
+++ String/string_format.py
@@ -2,5 +2,30 @@ drink = "coffee"
 drinkAmount = 4
 
 string = "Today I had {0} cups of {1}!".format(drinkAmount, drink)
-
 print(string)
+
+string = "Bananas: {b}, Apples: {a}, Pears: {p}".format(a = 3, p = 31, b = 12)
+print(string)
+
+string = "A {animal} has {0} arms and {1} legs!".format(2, 2, animal = "human")
+print(string)
+
+align_string = "Alignment!"
+#Right Alignment, 20 chars
+string = "{:>20}".format(align_string)
+print(string)
+
+#Left Alignment, 20 chars
+string = "{:<20}".format(align_string)
+print(string)
+
+number = 74
+#Base N output
+#Base 2, binary
+print("Binary {:b}".format(number))
+
+#Base 16, hexadecimal
+print("Hexadecimal {:x}".format(number))
+
+#Base 8, octal
+print("Octal {:o}".format(number))
